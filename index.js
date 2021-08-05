9c61e5d1262e8695080773c17d12f444046ed03e
unknown <xavier.saunier@hotmail.fr>
2021-08-05 16:44:46 +0200

    cors

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.json({ message: "Here comes marvel API" });
