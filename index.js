cfea74514343882d958a6541095909c3b8439046
unknown <xavier.saunier@hotmail.fr>
2021-08-06 11:48:26 +0200

    character route

--- index.js
+++ index.js
@@ -10,16 +10,13 @@ app.get("/", (req, res) => {
   res.json({ message: "Here comes marvel API" });
 });
 
-app.get("/comics", async (req, res) => {
-  try {
-    const response = await axios.get(
-      `https://lereacteur-marvel-api.herokuapp.com/comics?apiKey=${process.env.MARVEL_API_KEY}`
-    );
-    console.log(response.data);
-    res.json(response.data);
-  } catch (error) {
-    res.status(400).json({ message: error.message });
-  }
+const comicRoute = require("./routes/comics");
+app.use(comicRoute);
+const characterRoute = require("./routes/characters");
+app.use(characterRoute);
+
+app.all("*", (req, res) => {
+  res.status(400).json({ message: "page not found" });
 });
 
 app.listen(process.env.PORT, () => {
---
+++ routes/comics.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const router = express.Router();
+const axios = require("axios");
+
+router.get("/comics", async (req, res) => {
+  try {
+    const response = await axios.get(
+      `https://lereacteur-marvel-api.herokuapp.com/comics?apiKey=${process.env.MARVEL_API_KEY}`
+    );
+    console.log(response.data);
+    res.json(response.data);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+module.exports = router;
